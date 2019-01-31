44a5101c5186b9fe16ca4b0f5f0e172db60ea4ba
polaris2002 <ucity2002@fwdnet.co.kr>
2019-01-31 15:21:04 +0900

    modify seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,39 +14,38 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Company::class, function (Faker $faker) {
+	$faker = \Faker\Factory::create('ko_KR');
     return [
-        'CP_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'CP_SEQ' => $faker->unique()->randomNumber($nbDigits = 3),
+	'CP_ID' => $faker->numberBetween(0, 9999999999),
+        'CP_SEQ' => $faker->numberBetween(0, 99),
         'CP_NM' => $faker->company,
-        'CP_REG_NO' => $faker->unique()->randomNumber($nbDigits = 15),
+        'CP_REG_NO' => $faker->numberBetween(0, 9999999999),
         'CP_CEO' => $faker->name,
-        'CP_COR_NO' => $faker->unique()->randomNumber($nbDigits = 15),
+        'CP_COR_NO' => $faker->numberBetween(0, 9999999999),
         'CP_TEL' => $faker->phoneNumber,
         'CP_FAX' => $faker->phoneNumber,
         'CP_ADD' => $faker->address,
-        'INPUT_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'INPUT_DATE' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
-
+        'INPUT_ID' => $faker->numberBetween(0, 9999999999),
     ];
 });
 
 $factory->define(App\User::class, function (Faker $faker) {
+	$faker = \Faker\Factory::create('ko_KR');
     return [
-        'CP_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'US_ID' => $faker->unique()->randomNumber($nbDigits = 30),
+        'CP_ID' => $faker->numberBetween(0, 9999999999),
+        'US_ID' => $faker->numberBetween(0, 9999999999),
         'US_PWD' => str_random(10),
         'US_NM' => $faker->name,
-        'US_AUTH' => str_random(3),
-        'INPUT_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'INPUT_DATE' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
-
+        'US_AUTH' => str_random(1),
+        'INPUT_ID' => $faker->numberBetween(0, 9999999999),
     ];
 });
 
 $factory->define(App\Account::class, function (Faker $faker) {
+	$faker = \Faker\Factory::create('ko_KR');
     return [
-        'CP_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'AC_ID' => $faker->unique()->randomNumber($nbDigits = 30),
+        'CP_ID' => $faker->numberBetween(0, 9999999999),
+        'AC_ID' => $faker->numberBetween(0, 9999999999),
         'AC_NM' => $faker->company,
         'AC_TEL' => $faker->phoneNumber,
         'AC_FAX' => $faker->phoneNumber,
@@ -54,64 +53,60 @@ $factory->define(App\Account::class, function (Faker $faker) {
         'AC_EMAIL' => $faker->email,
         'AC_ADD' => $faker->address,
         'AC_EMP' => $faker->name,
-        'INPUT_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'INPUT_DATE' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
+        'INPUT_ID' => $faker->numberBetween(0, 9999999999),
 
     ];
 });
 
 $factory->define(App\Car::class, function (Faker $faker) {
+	$faker = \Faker\Factory::create('ko_KR');
     return [
-        'CP_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'CA_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'CA_NO' => $faker->unique()->randomNumber($nbDigits = 20),
+        'CP_ID' => $faker->numberBetween(0, 9999999999),
+        'CA_ID' => $faker->numberBetween(0, 9999999999),
+        'CA_NO' => $faker->numberBetween(0, 9999999999),
         'CA_WK_PLACE' => $faker->city,
         'CA_CP_TEL' => $faker->phoneNumber,
         'CA_CP_ADD' => $faker->address,
         'CA_OWN_NM' => $faker->name,
-        'INPUT_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'INPUT_DATE' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
+        'INPUT_ID' => $faker->numberBetween(0, 9999999999),
 
     ];
 });
 
 $factory->define(App\Driver::class, function (Faker $faker) {
-    $date = Carbon::create(2000, 1, 1, 0, 0, 0);
+	$faker = \Faker\Factory::create('ko_KR');
     return [
-        'CP_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'DR_ID' => $faker->unique()->randomNumber($nbDigits = 30),
+        'CP_ID' => $faker->numberBetween(0, 9999999999),
+        'DR_ID' => $faker->numberBetween(0, 9999999999),
         'DR_NM' => $faker->name,
         'DR_TEL' => $faker->phoneNumber,
         'DR_HP' => $faker->phoneNumber,
-        'DR_JOIN_DATE' => $date->addWeeks(rand(1, 900))->format('YYYY-mm-dd'),
+        'DR_JOIN_DATE' => $faker->date($format = 'Y-m-d', $max ='now'),
         'DR_EMAIL' => $faker->email,
         'DR_ADD' => $faker->address,
-        'INPUT_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'INPUT_DATE' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
+        'INPUT_ID' => $faker->numberBetween(0, 9999999999),
 
     ];
 });
 
 $factory->define(App\Allocation::class, function (Faker $faker) {
-    $date = Carbon::create(2000, 1, 1, 0, 0, 0);
+	$faker = \Faker\Factory::create('ko_KR');
     return [
-        'CP_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'CA_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'DR_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'AC_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'DS_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'AL_DATE' => $date->addWeeks(rand(1, 900))->format('YYYY-mm-dd'),
+        'CP_ID' => $faker->numberBetween(0, 9999999999),
+        'CA_ID' => $faker->numberBetween(0, 9999999999),
+        'DR_ID' => $faker->numberBetween(0, 9999999999),
+        'AC_ID' => $faker->numberBetween(0, 9999999999),
+        'DS_ID' => $faker->numberBetween(0, 9999999999),
+        'AL_DATE' => $faker->date($format = 'Y-m-d', $max = 'now'),
         'AL_ITEM' => $faker->word,
-        'AL_QTY' => $faker->randomNumber($nbDigits = 3),
-        'AL_UNIT_COST' => $faker->randomNumber($nbDigits = 5),
+        'AL_QTY' => $faker->numberBetween(0, 9999),
+        'AL_UNIT_COST' => $faker->numberBetween(0, 9999999),
         'AL_LOAD_PLACE' => $faker->city,
         'AL_DISCHARGE_PLACE' => $faker->city,
-        'AL_DEMAND_AMT' => $faker->randomNumber($nbDigits = 6),
-        'AL_PAY_AMT' => $faker->randomNumber($nbDigits = 6),
-        'AL_FEE_RATE' => $faker->randomNumber($nbDigits = 2),
-        'AL_FEE' => $faker->randomNumber($nbDigits = 5),
-        'INPUT_ID' => $faker->unique()->randomNumber($nbDigits = 30),
-        'INPUT_DATE' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
-
+        'AL_DEMAND_AMT' => $faker->numberBetween(0, 99999999),
+        'AL_PAY_AMT' => $faker->numberBetween(0, 99999999),
+        'AL_FEE_RATE' => $faker->numberBetween(0, 99),
+        'AL_FEE' => $faker->numberBetween(0, 999999),
+        'INPUT_ID' => $faker->numberBetween(0, 9999999999),
     ];
 });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['middleware' => ['web']], function () {
+	Route::resource('companies', 'CompanyController');
+});
